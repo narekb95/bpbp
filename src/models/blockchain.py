fc3362c552f951035a20cd10d6e6161e4c4ea556
bojikian <bojikian@informatik.hu-berlin.de>
2024-06-27 17:47:42 +0200

    added blockchain

---
+++ src/models/blockchain.py
@@ -0,0 +1,19 @@
+class Header:
+    def __init__(self, version, previous_hash, merkle_root, timestamp, bits, nonce, hash, height = None, id = None, prev_id = None):
+        self.version = version
+        self.previous_hash = previous_hash
+        self.merkle_root = merkle_root
+        self.timestamp = timestamp
+        self.bits = bits
+        self.nonce = nonce
+        self.hash = hash
+        self.height = height
+        self.id = id
+        self.prev_id = prev_id
+
+class Blockchain:
+    def __init__(self, headers = None):
+        if headers == None:
+            self.headers = []
+        else:
+            self.headers = headers
